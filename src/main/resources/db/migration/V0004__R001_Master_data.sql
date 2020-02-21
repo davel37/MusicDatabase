1d1e82876fec9523365c927420fe9e6dc7d36992
David Laffey <laffeydavid@hotmail.com>
2020-02-21 16:23:56 +0100

    Database additions

---
+++ src/main/resources/db/migration/V0004__R001_Master_data.sql
@@ -0,0 +1,107 @@
+INSERT INTO artist (id, name,country) VALUES (2, 'The Murder Capital', 'Ireland');
+INSERT INTO artist (id, name,country) VALUES (3, 'Against All Logic', 'Chile');
+INSERT INTO artist (id, name,country) VALUES (4, 'Lana Del Ray', 'America');
+INSERT INTO artist (id, name,country) VALUES (5, 'Deafheaven', 'America');
+INSERT INTO artist (id, name,country) VALUES (6, 'Slowdive', 'England');
+INSERT INTO artist (id, name,country) VALUES (7, 'Four Tet', 'England');
+INSERT INTO artist (id, name,country) VALUES (8, 'Fontaines DC', 'Ireland');
+INSERT INTO artist (id, name,country) VALUES (9, 'Lali Puna', 'Germany');
+INSERT INTO artist (id, name,country) VALUES (10, 'The Arcade Fire', 'America');
+
+INSERT INTO album (id, name, year, artist_id) VALUES (1, 'The Joshua Tree', '1988-02-10', 1);
+INSERT INTO album (id, name, year, artist_id) VALUES (2, 'The Unforgettable Fire', '1986-02-11', 1);
+INSERT INTO album (id, name, year, artist_id) VALUES (3, 'Achtung Baby', '1991-03-11', 1);
+INSERT INTO album (id, name, year, artist_id) VALUES (4, 'When I have fears', '2019-09-09', 2);
+INSERT INTO album (id, name, year, artist_id) VALUES (5, 'Norman Fuckin Rockwell', '2019-06-05', 4);
+INSERT INTO album (id, name, year, artist_id) VALUES (6, 'Sunbather', '2015-08-15', 5);
+INSERT INTO album (id, name, year, artist_id) VALUES (7, 'New Bermuda', '2018-07-09', 5);
+INSERT INTO album (id, name, year, artist_id) VALUES (8, 'Ordinary Corrupt Decent Love', '2020-04-06', 5);
+INSERT INTO album (id, name, year, artist_id) VALUES (9, 'Tridecoder', '1995-01-03', 9);
+INSERT INTO album (id, name, year, artist_id) VALUES (10, 'Funeral', '2010-06-04', 10);
+INSERT INTO album (id, name, year, artist_id) VALUES (11, 'The Suburbs', '2014-04-03', 10);
+INSERT INTO album (id, name, year, artist_id) VALUES (12, 'Dogrel', '2019-05-07', 8);
+INSERT INTO album (id, name, year, artist_id) VALUES (13, 'Rounds', '1995-01-01', 7);
+INSERT INTO album (id, name, year, artist_id) VALUES (14, 'Slowdive', '1995-06-05', 6);
+
+INSERT INTO songs (id, title, album_id) VALUES (1,'Where the streets have no name', 1);
+INSERT INTO songs (id, title, album_id) VALUES (2,'I still havent found what Im looking for	', 1);
+INSERT INTO songs (id, title, album_id) VALUES (3,'With or without you', 1);
+INSERT INTO songs (id, title, album_id) VALUES (4,'Bullet the blue sky', 1);
+INSERT INTO songs (id, title, album_id) VALUES (5,'Running to stand still', 1);
+INSERT INTO songs (id, title, album_id) VALUES (6,'Red Hill Mining Town', 1);
+INSERT INTO songs (id, title, album_id) VALUES (7,'In Gods Counry', 1);
+INSERT INTO songs (id, title, album_id) VALUES (8,'Trip through Your wires', 1);
+INSERT INTO songs (id, title, album_id) VALUES (9,'One Tree Hill', 1);
+INSERT INTO songs (id, title, album_id) VALUES (10,'Exit', 1);
+
+INSERT INTO songs (id, title, album_id) VALUES (11,'Dream House', 6);
+INSERT INTO songs (id, title, album_id) VALUES (12,'Irresistable', 6);
+INSERT INTO songs (id, title, album_id) VALUES (13,'Sunbather', 6);
+INSERT INTO songs (id, title, album_id) VALUES (14,'Please Remember', 6);
+INSERT INTO songs (id, title, album_id) VALUES (15,'Vertigo', 6);
+INSERT INTO songs (id, title, album_id) VALUES (16,'Windows', 6);
+INSERT INTO songs (id, title, album_id) VALUES (17,'The Pecan Tree', 6);
+INSERT INTO songs (id, title, album_id) VALUES (18,'Brought to the water', 7);
+INSERT INTO songs (id, title, album_id) VALUES (19,'Luna', 7);
+INSERT INTO songs (id, title, album_id) VALUES (20,'Baby Blue', 7);
+INSERT INTO songs (id, title, album_id) VALUES (21,'Come Back', 7);
+INSERT INTO songs (id, title, album_id) VALUES (22,'Gifts for the earth', 7);
+INSERT INTO songs (id, title, album_id) VALUES (23,'6-0-3', 9);
+INSERT INTO songs (id, title, album_id) VALUES (24,'Rapariga Da Banheira', 9);
+INSERT INTO songs (id, title, album_id) VALUES (25,'Antena Trash', 9);
+INSERT INTO songs (id, title, album_id) VALUES (26,'System On', 9);
+INSERT INTO songs (id, title, album_id) VALUES (27,'Everywhere and all over', 9);
+INSERT INTO songs (id, title, album_id) VALUES (28,'Toca Discos', 9);
+INSERT INTO songs (id, title, album_id) VALUES (29,'Press my Tummy', 9);
+INSERT INTO songs (id, title, album_id) VALUES (30,'Fast Forward', 9);
+INSERT INTO songs (id, title, album_id) VALUES (31,'Superloado', 9);
+INSERT INTO songs (id, title, album_id) VALUES (32,'Zoo Station', 3);
+INSERT INTO songs (id, title, album_id) VALUES (33,'Even better than the real thing', 3);
+INSERT INTO songs (id, title, album_id) VALUES (34,'One', 3);
+INSERT INTO songs (id, title, album_id) VALUES (35,'Until the end of the world', 3);
+INSERT INTO songs (id, title, album_id) VALUES (36,'Whos going to ride your wild horses', 3);
+INSERT INTO songs (id, title, album_id) VALUES (37,'So cruel', 3);
+INSERT INTO songs (id, title, album_id) VALUES (38,'The Fly', 3);
+INSERT INTO songs (id, title, album_id) VALUES (39,'Mysterious Ways', 3);
+INSERT INTO songs (id, title, album_id) VALUES (40,'Trying to throw your arms around the world', 3);
+INSERT INTO songs (id, title, album_id) VALUES (41,'Ultra Violet', 3);
+INSERT INTO songs (id, title, album_id) VALUES (42,'Acrobat', 3);
+INSERT INTO songs (id, title, album_id) VALUES (43,'Love is Blindness', 3);
+
+INSERT INTO songs (id, title, album_id) VALUES (44,'Hands', 13);
+INSERT INTO songs (id, title, album_id) VALUES (45,'She Moves she', 13);
+INSERT INTO songs (id, title, album_id) VALUES (46,'First Thing', 13);
+INSERT INTO songs (id, title, album_id) VALUES (47,'My angel rocks back and forth', 13);
+INSERT INTO songs (id, title, album_id) VALUES (48,'Sprit Fingers', 13);
+INSERT INTO songs (id, title, album_id) VALUES (49,'Unspoken', 13);
+INSERT INTO songs (id, title, album_id) VALUES (50,'Chia', 13);
+INSERT INTO songs (id, title, album_id) VALUES (51,'As Serious as your life', 13);
+INSERT INTO songs (id, title, album_id) VALUES (52,'And they ALl Looked Brokenhearted', 13);
+INSERT INTO songs (id, title, album_id) VALUES (53,'Slow Jam', 13);
+
+
+INSERT INTO songs (id, title, album_id) VALUES (54,'For Everything', 4);
+INSERT INTO songs (id, title, album_id) VALUES (55,'More is Less', 4);
+INSERT INTO songs (id, title, album_id) VALUES (56,'Green and Blue', 4);
+INSERT INTO songs (id, title, album_id) VALUES (57,'Slowdance 1', 4);
+INSERT INTO songs (id, title, album_id) VALUES (58,'Slowdance 2', 4);
+INSERT INTO songs (id, title, album_id) VALUES (59,'On Twisted Ground', 4);
+INSERT INTO songs (id, title, album_id) VALUES (60,'Feeling fades', 4);
+INSERT INTO songs (id, title, album_id) VALUES (61,'Dont Cling to Life', 4);
+INSERT INTO songs (id, title, album_id) VALUES (62,'How the Streets adore me now', 4);
+INSERT INTO songs (id, title, album_id) VALUES (63,'Love Love Love', 4);
+
+
+
+INSERT INTO gigs (id, venue, country, isSoldOut, dateOfGig, artist_id) VALUES (1, 'Roisin Dubh', 'Ireland',FALSE,'2020-03-01', 8);
+INSERT INTO gigs (id, venue, country, isSoldOut, dateOfGig, artist_id) VALUES (2, 'Whelans', 'Ireland',TRUE,'2020-03-02', 8);
+INSERT INTO gigs (id, venue, country, isSoldOut, dateOfGig, artist_id) VALUES (3, 'Dolans Limerick', 'Ireland',FALSE,'2020-03-03', 8);
+INSERT INTO gigs (id, venue, country, isSoldOut, dateOfGig, artist_id) VALUES (4, 'De Barras', 'Ireland',TRUE,'2020-03-04', 8);
+INSERT INTO gigs (id, venue, country, isSoldOut, dateOfGig, artist_id) VALUES (5, 'Limelight', 'Ireland',FALSE,'2020-03-05', 8);
+
+INSERT INTO gigs (id, venue, country, isSoldOut, dateOfGig, artist_id) VALUES (6, '02 Manchester', 'England',FALSE,'2020-03-08', 8);
+INSERT INTO gigs (id, venue, country, isSoldOut, dateOfGig, artist_id) VALUES (7, '02 London', 'England',FALSE,'2020-03-09', 8);
+INSERT INTO gigs (id, venue, country, isSoldOut, dateOfGig, artist_id) VALUES (8, '02 Liverpool', 'England',FALSE,'2020-03-10', 8);
+INSERT INTO gigs (id, venue, country, isSoldOut, dateOfGig, artist_id) VALUES (9, '02 Leciester', 'England',FALSE,'2020-03-11', 8);
+INSERT INTO gigs (id, venue, country, isSoldOut, dateOfGig, artist_id) VALUES (10, '02 Bath', 'England',FALSE,'2020-03-12', 8);
+INSERT INTO gigs (id, venue, country, isSoldOut, dateOfGig, artist_id) VALUES (11, '02 Bristol', 'England',FALSE,'2020-03-13', 8);
