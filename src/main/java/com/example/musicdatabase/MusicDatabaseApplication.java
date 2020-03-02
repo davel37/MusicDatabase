82ab4cc9920d5c25aaf0b55c569d93e923538cde
David Laffey <laffeydavid@hotmail.com>
2020-03-02 19:43:46 +0100

    commit all changes

--- src/main/java/com/example/musicdatabase/MusicDatabaseApplication.java
+++ src/main/java/com/example/musicdatabase/MusicDatabaseApplication.java
@@ -2,8 +2,14 @@ package com.example.musicdatabase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+
+@EnableJpaAuditing
+@EnableScheduling
+@SpringBootApplication//(exclude = KafkaAutoConfiguration.class)
 public class MusicDatabaseApplication {
 
 	public static void main(String[] args) {
